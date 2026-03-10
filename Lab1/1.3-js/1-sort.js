4cf6d5479acf13a85afb185c01175a666705c099
K3therLOL <g3roylol@gmail.com>
2026-03-10 15:34:31 +0300

    windows 10 change

--- Lab1/1.3-js/1-sort.js
+++ Lab1/1.3-js/1-sort.js
@@ -3,5 +3,5 @@ let nums = inputString.trim().split(/\s+/)
     .map(str => Number(str))
     .filter(num => Number.isInteger(num));
 
-sorted = nums.sort((a, b) => a - b);
+sorted = nums.sort((a, b) => a - b); // (a > b) - (a < b)
 alert(sorted.join(" "));
