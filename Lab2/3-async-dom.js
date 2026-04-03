f66b37301dc3bd1add1b78dcacce9eb5ebf49408
K3therLOL <g3roylol@gmail.com>
2026-04-03 19:53:34 +0300

    async-dom script completed

--- Lab2/3-async-dom.js
+++ Lab2/3-async-dom.js
@@ -1,32 +1,30 @@
-const input = prompt("Введите все ссылки последовательно через пробел: ");
-const urls = input.trim().split(' ');
+const input = prompt("Enter all links sequentially separated by spaces: ");
+const urls = input.trim().split(/\s+/);
 
-function loadImage(url) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = url;
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Невозможно загрузить картинку по "${url}"`));
-    })
-}
+const fetchedPromises = urls.map((url) => {
+    return fetch(url)
+        .then((resp) => {
+            if (!resp.ok) {
+                throw new Error(`Impossible to load image by "${url}"`);
+            }
+            return resp.blob();
+        })
+        .then((blob) => {
+            const img = new Image();
+            img.src = URL.createObjectURL(blob);
+            document.body.appendChild(img);
+            return img;
+        })
+        .catch((err) => {
+            throw new Error(`Impossible to load image by "${url}"`);
+        })
+});
 
-let has_error = false;
-urls.forEach((url) => {
-    loadImage(url)
-    .then((img) => {
-        if (has_error) {
-            return;
-        }
-        document.body.appendChild(img);
-    })
-    .catch((err) => {
-        if (has_error) {
-            return;
-        }
-        has_error = true;
-        const paragraph = document.createElement("p");
-        const paragraphText = document.createTextNode(err.message);
-        paragraph.appendChild(paragraphText);
-        document.body.appendChild(paragraph);
-    })
+Promise.all(fetchedPromises)
+.catch((err) => {
+    document.body.replaceChildren();
+    const paragraph = document.createElement("p");
+    const paragraphText = document.createTextNode(err.message);
+    paragraph.appendChild(paragraphText);
+    document.body.appendChild(paragraph);
 });
